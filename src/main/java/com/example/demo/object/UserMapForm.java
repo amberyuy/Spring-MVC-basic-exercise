3f614d8615c6a49671507dfdf6b2847bd09c0515
amberyuy <yuywang@bu.edu>
2020-11-02 14:54:38 -0500

    1

---
+++ src/main/java/com/example/demo/object/UserMapForm.java
@@ -0,0 +1,23 @@
+package com.example.demo.object;
+
+import java.util.Map;
+
+
+public class UserMapForm {
+    private Map<String,User> users;
+
+    @Override
+    public String toString() {
+        return "UserMapForm{" +
+                "users=" + users +
+                '}';
+    }
+
+    public Map<String, User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Map<String, User> users) {
+        this.users = users;
+    }
+}
